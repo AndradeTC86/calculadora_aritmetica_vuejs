2a59ab1137ca9d42b3726e6194d7f5185af15ab1
AndradeTC86 <andrade-tc@uol.com.br>
2025-05-20 18:49:24 -0300

    If and Else com Vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
+
+const ctfl = "https://bstqb.qa/img/selos/s-ctfl.png"
+const ctal = "https://bstqb.qa/img/selos/s-ctal-ta.png"
+
+const possuiCTFL = true
+const possuiCTAL = false
+
 </script>
 
 <template>
   <h1>Calculadora Aritmética</h1>
+  <img v-if="possuiCTFL" :src="ctfl" alt="CTFL" />
+  <img v-else-if="possuiCTAL" :src="ctal" alt="CTAL" />
+  <h2 v-else>Não possui certificação</h2>
 </template>
 
 <style scoped>
+
+img {
+  max-width: 300px;
+}
+
 </style>
