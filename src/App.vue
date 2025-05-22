7b8079945dd5d973d9dfd7405c7416df42e235b3
AndradeTC86 <andrade-tc@uol.com.br>
2025-05-22 19:41:49 -0300

    Eventos com o VueJS

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <script setup>
+import { reactive } from 'vue'
 
 const ctfl = "https://bstqb.qa/img/selos/s-ctfl.png"
 const ctal = "https://bstqb.qa/img/selos/s-ctal-ta.png"
 
 const possuiCTFL = true
 const possuiCTAL = false
+
+const estado = reactive({
+  contador: 0
+})
+
+function incrementar(){
+  estado.contador++
+}
+
+function decrementar(){
+  estado.contador--
+}
+
+function alteraEmail(evento){
+  estado.email = evento.target.value
+}
 
 </script>
 
@@ -13,6 +30,15 @@ const possuiCTAL = false
   <img v-if="possuiCTFL" :src="ctfl" alt="CTFL" />
   <img v-else-if="possuiCTAL" :src="ctal" alt="CTAL" />
   <h2 v-else>Não possui certificação</h2>
+
+{{  estado.contador  }}
+<button type="button" @click="incrementar">+</button>
+<button type="button" @click="decrementar">-</button>
+
+<hr />
+{{ estado.email }}
+<input type="email" @keyup="alteraEmail" />
+
 </template>
 
 <style scoped>
